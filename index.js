812ee18ec8251721c9bdb005f03d491381f63cca
dlray89 <dlrayjr89@gmail.com>
2020-03-09 15:50:15 -0400

    post is working

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 //import express after download
 const express = require('express');
+//import shortid here
+const shortid = require('shortid')
 
 //set server to express as a function
 const server = express();
 
 //use the server on express
 server.use(express.json())
+
+
+//get users
+server.get('/api/users', (req,res) => {
+    res.status(200).json({users: "Have been retrieved"})
+})
+
+
+//post users/new
+
+
+server.post('/api/users', (req,res) => {
+const user = [
+    {
+        "id": 1,
+        "name": "David",
+        "bio": "Web Dev"
+    }
+]
+
+const userInfo = req.body
+user.push(userInfo)
+userInfo.id = shortid.generate()
+res.status(200).json(userInfo)
+})
+
 
 
 //set port up
