1168e8bc9b0333820a68c8de38422ebc4b17f44d
dlray89 <dlrayjr89@gmail.com>
2020-03-09 22:04:29 -0400

    Couldn't get the 500 error in the get request

--- index.js
+++ index.js
@@ -9,36 +9,43 @@ const server = express();
 //use the server on express
 server.use(express.json())
 
-
-//get users
-server.get('/api/users', (req,res) => {
-    res.status(200).json({users: "Have been retrieved"})
-})
-
-
-//post users/new
-
-
-server.post('/api/users', (req,res) => {
-const user = [
-    {
-        "id": 1,
-        "name": "David",
-        "bio": "Web Dev"
-    }
-]
-
-const userInfo = req.body
-user.push(userInfo)
-userInfo.id = shortid.generate()
-res.status(200).json(userInfo)
-})
-
-
-
 //set port up
 const port = 5000;
 
+
+let users = [
+    {
+        id: 1,
+        name:"David",
+        bio:"Web Dev"
+    }
+]
+
+server.post('/api/users', (req,res) => {
+const userInfo = req.body
+if(!req.body.name|| !req.body.bio) {
+    res.status(400).json({errorMessage: "Must enter a name and bio for user"})
+}
+users.push(userInfo)
+userInfo.id = shortid.generate()
+res.status(200).json(userInfo)
+
+})
+
+server.get('api/users', (req,res) => {
+ 
+    if(!req.body.name || !req.body.bio ) {
+        res.status(500).json({ErrorMessage: "The users information could not be retrived"})
+    }
+    
+})
+
+
+// const userInfo = req.body
+// user.push(userInfo)
+// userInfo.id = shortid.generate()
+// res.status(200).json(userInfo)
+//get users
 //listen to server to make sure its working
 server.listen(port, () => 
 console.log(`n\ ** This is server http://localhost:${port} ** \n`))
